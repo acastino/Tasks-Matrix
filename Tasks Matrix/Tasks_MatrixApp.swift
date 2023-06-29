dfc098c4e213eb8ee5348d7daab89127d56ef85e
Amante Castino <a.castino@gmail.com>
2023-06-29 16:41:47 +0800

    Initial Commit

---
+++ Tasks Matrix/Tasks_MatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tasks_MatrixApp.swift
+//  Tasks Matrix
+//
+//  Created by Amante Castino on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Tasks_MatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
